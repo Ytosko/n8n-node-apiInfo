eb290adf1e25400b4023ce5790ce06218afafcb1
Saiki Sarkar <ytosko@ytosko.dev>
2025-12-29 03:22:48 +0600

    Patched

--- credentials/ApiInfoApi.credentials.ts
+++ credentials/ApiInfoApi.credentials.ts
@@ -1,4 +1,5 @@
 import {
+    IAuthenticateGeneric,
     ICredentialType,
     INodeProperties,
 } from 'n8n-workflow';
@@ -15,4 +16,12 @@ export class ApiInfoApi implements ICredentialType {
             default: '',
         },
     ];
+    authenticate: IAuthenticateGeneric = {
+        type: 'generic',
+        properties: {
+            qs: {
+                token: '={{$credentials.accessToken}}',
+            },
+        },
+    };
 }
